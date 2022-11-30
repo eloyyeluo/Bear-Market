1b2f928dca8e61ae23a9ce94de2489ad4d8cc9e4
Eloy Gustavo Ye Luo <75350380+Eloyye@users.noreply.github.com>
2022-11-30 14:21:48 -0800

    add .gitignore and starter code

---
+++ Bear Market/Bear_MarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bear_MarketApp.swift
+//  Bear Market
+//
+//  Created by Eloy Ye Luo on 11/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Bear_MarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
