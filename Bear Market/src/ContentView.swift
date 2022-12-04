541c1ad0cf184e959ef108ed887b3c8114cd7213
Eloy Gustavo Ye Luo <yeluo.eloy@gmail.com>
2022-12-04 10:39:05 -0800

    restructured the project

---
+++ Bear Market/src/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Summary
+//
+//  Created by Canhui Huang on 12/4/22.
+//
+import SwiftUI
+
+class Items: Identifiable {
+    
+    var itemName: String
+    var stock: String
+    
+    init(itemName: String, stock: String) {
+        self.itemName = itemName
+        self.stock = stock
+    }
+}
+
+struct ContentView: View {
+    @State var tempItemName: String = ""
+    @State var locker: String = "A5"
+    @State var code: String = "4396"
+    
+    @State var list = [
+        Items(itemName: "Apples", stock: ""),
+        Items(itemName: "Bananas", stock: ""),
+        Items(itemName: "Broccoli", stock: ""),
+        Items(itemName: "Salmon", stock: ""),
+        Items(itemName: "SPAM", stock: "")
+    ]
+        
+    var body: some View {
+        NavigationView {
+            List {
+                HStack{
+                    Text("Your Locker: ")
+                    Text(locker)
+                }
+                
+                HStack{
+                    Text("Your Code: ")
+                    Text(code)
+                }
+                
+                HStack{
+                    Text("Items: ")
+                }
+                    ForEach(list) {listItem in
+                        CustomCell(itemName: listItem.itemName)
+                    }
+                
+            }.navigationTitle("Summary")
+        }
+    }
+}
+
+struct ContentView_Preview: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
