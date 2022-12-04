2bf3679736793bfdbd68ec157881a860d1fef30f
Eloy Gustavo Ye Luo <yeluo.eloy@gmail.com>
2022-12-04 10:55:23 -0800

    Connect Firebase to application

---
+++ Bear Market/src/Views/AuthenticationView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationView.swift
+//  Bear Market
+//
+//  Created by Eloy Ye Luo on 12/4/22.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthenticationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthenticationView()
+    }
+}
