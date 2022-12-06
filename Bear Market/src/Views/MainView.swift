18471d2c1301b73fb11c563a5c65702c9f1ed9d4
Eloy Gustavo Ye Luo <yeluo.eloy@gmail.com>
2022-12-05 17:31:41 -0800

    demo integrating welcome and login

---
+++ Bear Market/src/Views/MainView.swift
@@ -0,0 +1,38 @@
+//
+//  MainView.swift
+//  Bear Market
+//
+//  Created by Eloy Ye Luo on 12/5/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationView {
+            TabView{
+                SummaryView()
+                    .tabItem {
+                        Image(systemName: "book.circle")
+                                            Text("Summary")
+                    }
+                CartView()
+                    .tabItem {
+                        Image(systemName: "cart")
+                                            Text("Cart")
+                    }
+                FoodPantryView()
+                    .tabItem{
+                        Image(systemName: "person.crop.circle")
+                                            Text("Food Pantry")
+                    }
+            }
+        }
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
